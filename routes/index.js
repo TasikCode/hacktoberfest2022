1688412dd745c0397a5e8bb1255c0a3259ab52eb
Dagus Wijaya <dagus.wijaya@gmail.com>
2020-09-29 15:24:48 +0700

    switch to pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tasikcode', message: 'Tasikmalaya Hacktoberfest 2020' });
 });
 
 module.exports = router;
